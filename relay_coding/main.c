8ee6b088feae998acb7a02631db06e0f56447316
liyusang1 <liyusang1@naver.com>
2020-05-18 21:29:32 +0900

    commit

--- relay_coding/main.c
+++ relay_coding/main.c
@@ -2,7 +2,10 @@
 
 int main(){
 
+        char name;
 	printf("Hello world\n");
-
+        printf("my name is yusang")"
+        printf("what is your name ? : )"
+        scanf("%s,name"); 
 	return 0;
 }
