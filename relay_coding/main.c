22fb74abb83db9cd7d4ae44c73ed3ff71eab86bc
seung <kevin0969@naver.com>
2020-05-24 20:04:43 +0900

    Added age in main.c

--- relay_coding/main.c
+++ relay_coding/main.c
@@ -9,7 +9,9 @@ int main(){
         printf("what is your name ? : \n");
 	printf("my name is gyeonghoon\n");
 	printf("what is your name ? : \n");
-        scanf("%s,name");
- 
+        scanf("%s",name);
+	printf("How old are you?\n");
+		int age;
+		scanf("%d",&age);
 	return 0
 }
