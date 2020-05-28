6bc15c3e61c7c232afa734940f9996d2772879ac
jisun <sall75@naver.com>
2020-05-28 23:12:42 +0900

    add main.c

--- relay_coding/main.c
+++ relay_coding/main.c
@@ -3,13 +3,7 @@
 
 int main(){
 
-        char name;
-	printf("Hello world\n");
-        printf("my name is yusang\n");
-        printf("what is your name ? : \n");
-	printf("my name is gyeonghoon\n");
-	printf("what is your name ? : \n");
-        scanf("%s,name");
- 
-	return 0
+        
+	printf("Hello~!^^\n"); 
+	
 }
