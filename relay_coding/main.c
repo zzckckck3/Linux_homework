44f783ab3839f9a91ba56e6fd5c3e9f7f8705655
lbh <soulb9@naver.com>
2020-06-01 10:40:25 +0900

    add timer in main.c

--- relay_coding/main.c
+++ relay_coding/main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <time.h>
 
 int main(){
 
@@ -28,6 +28,13 @@ int main(){
 	printf("Hello~~!!!^^\n");
 	
 	printf("Hi~~!!\n");
+
+	printf("Hi there~! Today is the duedate of this assignment.\n");
+	if((tm.tm_year + 1900 <= 2020) && (tm.tm_mon + 1 <= 6) && (tm.tm_mday < 2)){
+		printf("now is %d-%d-%d %d:%d:%d\n",tm.tm_year+1900, tm.tm_mon+1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+		printf("And the time remain is %d: %d: %d\n",24-tm.tm_hour, 60-tm.tm_min, 60-tm.tm_sec);
+	}
+	else		printf("Ugamigun jane.. Imi yaejunae ggeutnatdane..");
 
 	return 0
 
